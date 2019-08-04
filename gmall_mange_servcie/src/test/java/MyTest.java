4da392443b505672919240447e2b6948af431ff1
liyanss <lyl50100976@gmail.com>
2019-08-04 17:28:54 +0800

    一点零

---
+++ gmall_mange_servcie/src/test/java/MyTest.java
@@ -0,0 +1,25 @@
+public class MyTest {
+    public static void main(String[] args) {
+        int hello = new Myclass().getHello();
+        System.out.println(hello);
+    }
+}
+class Myclass{
+    public int  getHello() {
+        Integer a=null;
+
+        try {
+            a = new Integer(20);
+            //
+            // a=10/0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("4444444400000");
+            //a=20;
+        }
+        return a;
+    }
+
+}
